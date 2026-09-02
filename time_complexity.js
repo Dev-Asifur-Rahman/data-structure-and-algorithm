4ce176ce806bfa948e606eb0dbd2a45167226499
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-09-02 23:50:00 +0600

    time complexity o1 and o(n) learned

--- time_complexity.js
+++ time_complexity.js
@@ -1,1 +1,23 @@
-console.log('Hello')
+// check notes q1
+
+function sumFnOne(n) {
+    let sum = 0
+    for(let i = 0; i<=n; i++){
+        sum += i
+    }
+    return sum
+}
+
+// time complexity O(n)
+
+function sumFnTwo(n){
+    return n*(n+1) / 2
+}
+
+// time complexity O(1)
+
+
+
+
+// console.log(sumFnOne(10))
+// console.log(sumFnTwo(10))
